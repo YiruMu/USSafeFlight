152dde6b011ceb7527b778f92c55530a079d2b39
Yiru Mu <yiru.mu@ufl.edu>
2021-11-26 00:35:34 -0500

    updated

--- Graph.h
+++ Graph.h
@@ -1,5 +1,6 @@
 #include <string>
 #include <map>
+#include<vector>
 using namespace std;
 
 class Graph{
@@ -8,16 +9,153 @@ private:
     map<string, vector<pair<string,int>>> graph;
 public:
     void insertEdge(string from, string to, int weight);
-    bool isEdge(int from, int to);
-    int getWeight(int from, int to);
-    vector<int> getAdjacent(int vertex);
+    bool isEdge(string from, string to);
+    int getWeight(string from, string to);
+    vector<string> getAdjacent(string vertex);
     void printGraph();
     int getIndegree(string vertex);
     int getOutdegree(string vertex);
     int costliestEdge();
-    vector<string> dijkstra(string originState, string destState);
-    vector<string> bellmanFord(string origin, string destState);
 
 };
+
+void Graph::insertEdge(string from, string to, int weight)
+{
+    /*
+         insertEdge() adds a new edge between the from and to vertex.
+    */
+    graph[from].push_back(make_pair(to, weight));
+    if (graph.find(to) == graph.end())
+        graph[to]={};
+}
+
+bool Graph::isEdge(string from, string to)
+{
+    /*
+        if there is an edge between the from and to vertex
+    */
+    if (graph.find(from)!= graph.end())
+    {
+        for (int i =0; i<graph[from].size();i++)
+        {
+            if(graph[from].at(i).first==to)
+                return true;
+        }
+    }
+    return false;
+}
+
+int Graph::getWeight(string from, string to)
+{
+    int weight =0;
+    if (graph.find(from)!= graph.end())
+    {
+        for (int i =0; i<graph[from].size();i++)
+        {
+            if(graph[from].at(i).first==to)
+                weight = graph[from].at(i).second ;
+        }
+    }
+
+    return weight ;
+}
+
+vector<string> Graph::getAdjacent(string vertex)
+{
+
+    vector<string> adjacent;
+    if (graph.find(vertex) != graph.end())
+    {  for (int i =0; i<graph[vertex].size();i++)
+        {
+            adjacent.push_back(graph[vertex].at(i).first);
+        }
+
+
+        // sort the vector; might not needed for strings
+        for (int i =1;i<adjacent.size(); i++)
+        {
+            string key = adjacent.at(i);
+            int j = i-1;
+            while (key<adjacent.at(j)&& j>=0)
+            {
+                adjacent.at(j+1) = adjacent.at(j);
+                j--;
+            }
+            adjacent.at(j+1) = key;
+        }
+    }
+    return adjacent;
+}
+
+void Graph::printGraph()
+{
+    auto itr = graph.begin();
+    for (; itr != graph.end(); ++ itr)
+    {
+        vector<string> temp = getAdjacent(itr->first);
+        cout<<itr->first;
+        if (temp.size()!=0)
+            cout<<" ";
+        for (int i =0; i<temp.size();i++)
+        {
+            cout<<temp.at(i);
+            if (i!= temp.size()-1)
+                cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int Graph::getIndegree(string vertex)
+{
+
+    int degree = 0;
+    auto itr = graph.begin();
+    for (; itr != graph.end(); ++ itr)
+    {
+
+        for (int i =0; i<graph[itr->first].size();i++)
+        {
+            if (graph[itr->first].at(i).first == vertex )
+            {
+                degree++;
+            }
+        }
+    }
+
+    return degree;
+}
+
+int Graph::getOutdegree(string vertex)
+{
+
+    int degree = 0;
+    if (graph.find(vertex) != graph.end())
+    {
+        degree = graph[vertex].size();
+    }
+
+    return degree;
+}
+
+int Graph::costliestEdge()
+{
+
+    auto itr = graph.begin();
+    int largest = graph[itr->first].at(0).second ;
+    for (; itr != graph.end(); ++ itr)
+    {
+
+        for (int i =0; i<graph[itr->first].size();i++)
+        {
+            if (graph[itr->first].at(i).second > largest )
+            {
+                largest = graph[itr->first].at(i).second;
+            }
+        }
+    }
+    return largest;
+
+}
 
 
--- main.cpp
+++ main.cpp
@@ -1,18 +1,50 @@
 #include <iostream>
 #include "Graph.h"
+#include <vector>
+#include <string>
 using namespace std;
-// Load data function here
+
+// Load data function and find the best paths functions here
+void LoadData(vector<Graph>& flights, string path);
+vector<string> dijkstra(const Graph& graph, string start);
+vector<string> bellmanFord(const Graph& graph, string start);
+
 int main() {
     /* create a vector of 31 graphs using load data function
      contains a while loop, it will keep running until the user inputs -1
      command prompt:
-        Welcome to US Safe Flight, enter -1 to terminate the program
+        Welcome to US Safe Flight, enter 'No' to terminate the program
         What day of the month do you intent to travel?
         What is your starting location?
         What is your destination?
      output:
         Your recommended flight(s) are shown below
      */
+    vector<Graph> flights (31, Graph());
+    // we can call pass the reference of flights into the load function above and load the data
+
+    string command = "Yes";
+    string day ="";
+    string start = "";
+    string des = "";
+    while(command=="Yes")
+    {
+        cout<<"Welcome to US Safe Flight! We are here to help you find the best flight(s)"<<endl;
+        /*
+         * Take in commands and make sure they are valid; If they are valid, we can change the string to
+         * match the data in csv, e.g. 'Los Angeles, CA'
+         */
+        cout<<"Would you like to continue (Yes/No)?"<<endl;
+        cin>>command;
+        while (command != "Yes" && command != "No")
+        {
+            cout<<"Invalid input. Try again."<<endl;
+            cin>>command;
+        }
+
+
+
+    }
 
     return 0;
 }
