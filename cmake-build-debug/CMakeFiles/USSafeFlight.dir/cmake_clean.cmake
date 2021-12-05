d4e83d258c3d01f40bbf9f6b6f527d7a54d2e52f
Yiru Mu <yiru.mu@ufl.edu>
2021-12-05 12:05:48 -0500

    Revert "updated"
    This reverts commit 55d72f2bc676b8aa20fbb4e388f8e0a1f4080aec.

---
+++ cmake-build-debug/CMakeFiles/USSafeFlight.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/USSafeFlight.dir/main.cpp.o"
+  "USSafeFlight"
+  "USSafeFlight.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/USSafeFlight.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
